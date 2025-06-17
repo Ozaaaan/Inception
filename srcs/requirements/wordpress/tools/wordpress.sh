3b65beabe5185c4a3433a72ac75f3e3b8611cc3d
ozanvm <ozan@vm.com>
2025-06-17 11:41:50 +0000

    working ???

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,45 +1,52 @@
 #!/bin/bash
+
+# Préparation PHP-FPM
 mkdir -p /run/php/
-sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
+sed -i 's|listen = /run/php/php7.4-fpm.sock|listen = 9000|' /etc/php/7.4/fpm/pool.d/www.conf
 
-if [ -f "/var/www/html/wordpress/wp-config.php" ]; then
-	exec "$@";
-	exit 0;
+# Installation de WP-CLI si manquant
+if ! command -v wp &> /dev/null; then
+    wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    chmod +x wp-cli.phar
+    mv wp-cli.phar /usr/local/bin/wp
 fi
 
-if [ ! -d "/usr/local/bin/wp/" ]; then
-	wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-	chmod +x wp-cli.phar
-	mv wp-cli.phar /usr/local/bin/wp
-fi
-
-if [ ! -d "/var/www/html/wordpress" ]; then
+# Installation WordPress si non encore fait
+if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
     mkdir -p /var/www/html/wordpress
+    cd /var/www/html/wordpress
+
+    echo "[INFO] Téléchargement de WordPress..."
+    wp core download --allow-root
+
+    echo "[INFO] Création du fichier wp-config.php..."
+    wp config create \
+        --dbname=$WORDPRESS_DB_NAME \
+        --dbuser=$WORDPRESS_DB_USER \
+        --dbpass=$WORDPRESS_DB_PASSWORD \
+        --dbhost=$WORDPRESS_DB_HOST \
+        --path='/var/www/html/wordpress' \
+        --allow-root
+
+    echo "[INFO] Installation de WordPress..."
+    wp core install \
+        --url=$DOMAIN_NAME \
+        --title="$WORDPRESS_TITLE" \
+        --admin_user=$WORDPRESS_ADMIN_USER \
+        --admin_password=$WORDPRESS_ADMIN_PASSWORD \
+        --admin_email=$WORDPRESS_ADMIN_EMAIL \
+        --allow-root
+
+    echo "[INFO] Création de l'utilisateur secondaire..."
+    wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL \
+        --user_pass=$WORDPRESS_USER_PASSWORD \
+        --role=author \
+        --allow-root
 fi
 
-cd /var/www/html/wordpress
-rm -rf /var/www/html/wordpress/*
+# Permissions
+chown -R www-data:www-data /var/www/html/wordpress
+chmod -R 755 /var/www/html/wordpress
 
-wp core download --allow-root
-
-wp config create	--allow-root \
-			--dbname=$DATABASE_NAME \
-			--dbuser=$DATABASE_ROOT \
-			--dbpass=$DATABASE_ROOT_PASSWORD \
-			--dbhost=mariadb:3306 \
-			--path='/var/www/html/wordpress/'
-
-wp core install		--allow-root \
-		        --url=$WORDPRESS_URL \
-			--title=$WORDPRESS_TITLE \
-			--admin_user=$WORDPRESS_ADMIN_USER \
-			--admin_password=$WORDPRESS_ADMIN_PASSWORD \
-			--admin_email=$WORDPRESS_ADMIN_EMAIL
-
-wp user create	--allow-root \
-		$WORDPRESS_SUB_USER \
-		$WORDPRESS_SUB_EMAIL \
-		--role=$WORDPRESS_SUB_ROLE \
-		--user_pass=$WORDPRESS_SUB_PASSWORD
-
+echo "[INFO] Lancement de PHP-FPM..."
 exec "$@"
