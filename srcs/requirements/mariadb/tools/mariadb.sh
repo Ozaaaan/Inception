cc881be0f1d1d01ace67ebd6e5b46ee33a88fdfc
Ozaaaan <ozan-demir@hotmail.fr>
2025-06-24 15:08:05 +0200

    buuu

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -2,15 +2,16 @@
 
 mysqld_safe &
 
-sleep 5
+until mysqladmin ping --silent; do
+        echo "Attente que MariaDB soit prêt..."
+        sleep 2
+done
 
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%';"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 mysql -e "FLUSH PRIVILEGES;"
-
-sleep 5
 
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 
