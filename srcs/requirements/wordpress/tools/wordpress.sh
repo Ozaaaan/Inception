b7a7c60bb1f1264f7368564fe15b611c338f045c
Ozaaaan <ozan-demir@hotmail.fr>
2025-06-24 15:38:12 +0200

    finallll

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Préparation PHP-FPM
+# Preparation PHP-FPM
 mkdir -p /run/php/
 sed -i 's|listen = /run/php/php7.4-fpm.sock|listen = 9000|' /etc/php/7.4/fpm/pool.d/www.conf
 
@@ -16,15 +16,15 @@ if ! command -v wp &> /dev/null; then
     mv wp-cli.phar /usr/local/bin/wp
 fi
 
-# Installation WordPress si non encore fait
+# Installation WordPress si pas installer correctement
 if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
     mkdir -p /var/www/html/wordpress
     cd /var/www/html/wordpress
 
-    echo "[INFO] Téléchargement de WordPress..."
+    echo "Telechargement de WordPress..."
     wp core download --allow-root
 
-    echo "[INFO] Création du fichier wp-config.php..."
+    echo "Creation du fichier wp-config.php..."
     wp config create \
         --dbname=$WORDPRESS_DB_NAME \
         --dbuser=$WORDPRESS_DB_USER \
@@ -33,7 +33,7 @@ if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
         --path='/var/www/html/wordpress' \
         --allow-root
 
-    echo "[INFO] Installation de WordPress..."
+    echo "Installation de WordPress..."
     wp core install \
         --url=$DOMAIN_NAME \
         --title="$WORDPRESS_TITLE" \
@@ -42,7 +42,7 @@ if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
         --admin_email=$WORDPRESS_ADMIN_EMAIL \
         --allow-root
 
-    echo "[INFO] Création de l'utilisateur secondaire..."
+    echo "Creation de l'utilisateur secondaire..."
     wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL \
         --user_pass=$WORDPRESS_USER_PASSWORD \
         --role=author \
@@ -53,5 +53,5 @@ fi
 chown -R www-data:www-data /var/www/html/wordpress
 chmod -R 755 /var/www/html/wordpress
 
-echo "[INFO] Lancement de PHP-FPM..."
+echo "Lancement de PHP-FPM..."
 exec "$@"
